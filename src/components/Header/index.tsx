d37131b1c4975b174e3c8ff2317e0b07b6c6b305
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-09-01 15:44:10 -0300

    Criando um portal acessivel

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,29 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog"
 
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
     return (
-       <HeaderContainer>
-        <HeaderContent>
-            <img src={logoImg} alt="Logo do ignite" />
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImg} alt="Logo do ignite" />
 
-            <NewTransactionButton>Nova transação</NewTransactionButton>
-        </HeaderContent>
-       </HeaderContainer> 
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Content>
+                        <Dialog.Title>Vova transação</Dialog.Title>
+
+                        <Dialog.Close />
+                    </Dialog.Content>
+                    <Dialog.Portal>
+                        <Dialog.DialogPortal />
+                    </Dialog.Portal>
+                </Dialog.Root>
+            </HeaderContent>
+        </HeaderContainer>
     )
 }
